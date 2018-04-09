c86ac8124571bb17b5c18cda58dcfa6a559a6ffb
Dominic Bowden <tedle@blon.se>
2018-04-09 00:29:38 -0700

    Add fonts and SASS import resolutions

--- web-client/webpack.config.js
+++ web-client/webpack.config.js
@@ -15,6 +15,7 @@ module.exports = {
     resolve: {
         alias: {
             components: path.resolve(__dirname, "src/components"),
+            styles: path.resolve(__dirname, "src/styles"),
             utils: path.resolve(__dirname, "src/utils")
         }
     },
