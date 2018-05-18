dcf095c5f127f025abd031ce03715bb00d64cae3
Dominic Bowden <tedle@blon.se>
2018-05-18 16:38:25 -0700

    Version 0.1.0

--- bot/uita/__init__.py
+++ bot/uita/__init__.py
@@ -1,6 +1,6 @@
 """Music bot for Discord that can be controlled via websockets"""
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 __author__ = "Dominic Bowden"
 __copyright__ = "2018, Dominic Bowden"
 __license__ = "ISC"
